3235bc55e881e8edfb0a3868a8adb934fe252714
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-06-10 00:56:03 +0900

    [add] ROSのパッケージを追加
    Cam -> ObjectのTFを発行するノードを作った

---
+++ script/run_extrinsic_calib.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+
+import rospy
+# Camera
+from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from kinect_calibration.extrinsic_calibration import ExtrinsicCalibration
+# TF
+import tf
+import tf2_ros
+import geometry_msgs.msg
+import turtlesim.msg
+
+class getTFbyExtrinsicCalib(object):
+    
+    def __init__(self):
+        self.caminfo_sub_topic = rospy.get_param('~cam_info_topic')
+        self.image_sub_topic = rospy.get_param('~image_topic')
+        self.frame = rospy.get_param('~frame_name')
+        self.object_frame = rospy.get_param('~object_frame_name')
+        self.row = rospy.get_param('~row_num')
+        self.column = rospy.get_param('~column_num')
+        self.size = rospy.get_param('~size')
+        self.target_type = rospy.get_param('~target_type')
+
+        self.ex_calib = ExtrinsicCalibration(self.row, self.column, self.size, None, None)
+        self.bridge = CvBridge()
+        
+        image_sub = rospy.Subscriber(self.image_sub_topic, Image, self.imageCallback)
+        info_sub = rospy.Subscriber(self.caminfo_sub_topic, CameraInfo, self.camInfoCallback)
+    
+    def imageCallback(self, message):
+        img = self.bridge.imgmsg_to_cv2(message, "bgr8")
+        ret = self.ex_calib.calibrate(img, self.target_type)
+        if ret is True :
+            br = tf2_ros.TransformBroadcaster()
+            t = geometry_msgs.msg.TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = self.frame
+            t.child_frame_id = self.object_frame
+
+            t.transform.translation.x = self.ex_calib.tvecs[0].ravel()
+            t.transform.translation.y = self.ex_calib.tvecs[1].ravel()
+            t.transform.translation.z = self.ex_calib.tvecs[2].ravel()
+            q = tf.transformations.quaternion_from_euler(self.ex_calib.roll, self.ex_calib.pitch, self.ex_calib.yaw)
+            t.transform.rotation.x = q[0]
+            t.transform.rotation.y = q[1]
+            t.transform.rotation.z = q[2]
+            t.transform.rotation.w = q[3]
+
+            br.sendTransform(t)
+            rospy.loginfo("Get a Image")
+        else :
+            rospy.logwarn("Failed to get Pose")
+        
+    def camInfoCallback(self, message):
+        mtx = np.float64([[message.K[0], message.K[1], message.K[2]],
+                          [message.K[3], message.K[4], message.K[5]],
+                          [message.K[6], message.K[7], message.K[8]]])
+        dist = list(message.D)
+        self.ex_calib.mtx = np.float64(mtx)
+        self.dist = np.float64(dist)
+    
+if __name__ == "__main__":
+        
+    rospy.init_node('camera_extrinsic_calibrater')
+    node = getTFbyExtrinsicCalib()
+    rospy.spin()
--- src/kinect_calibration/extrinsic_calibration.py
+++ src/kinect_calibration/extrinsic_calibration.py
@@ -3,6 +3,7 @@
 import cv
 import cv2
 import numpy as np
+import math
 import glob
 
 class ExtrinsicCalibration(object):
@@ -13,7 +14,7 @@ class ExtrinsicCalibration(object):
         # == Point Informations ==
         self.row        = row      # Row number of points
         self.column     = column   # Column number of points
-        self.size       = size*2.0 # Distans between points (unit is [m])
+        self.size       = size+size # Distans between points (unit is [m])
         # Criteria of Points
         self.criteria   = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         # Point numbers calculate from Row and Column numbers
@@ -23,8 +24,18 @@ class ExtrinsicCalibration(object):
             for y in range(3):
                 self.objp[x][y] *= self.size
         # == Camera Informations ==
-        self.mtx  = mtx  # Camera Matrix (3x3)
-        self.dist = dist # Dist Coeffs (4,5 ~ 8 arguments)
+        if mtx is None :
+            self.mtx = np.float64([[403.533846, 0.0, 153.470313],
+                                   [0.0, 399.684149, 113.18246],
+                                   [0.0, 0.0, 1.0]])
+        else :
+            self.mtx  = mtx  # Camera Matrix (3x3)
+            
+        if dist is None :
+            self.dist = np.float64([0.08348799999999999, -0.196376, 0.00032, 0.001903, 0.0])
+        else:
+            self.dist = dist # Dist Coeffs (4,5 ~ 8 arguments)
+
         # == OpenCV arguments ==
         self.use_window_flg = False # Flag whether use window or not
         self.rvecs          = None  # Rotete Vector
@@ -44,7 +55,7 @@ class ExtrinsicCalibration(object):
             ret, self.points = cv2.findChessboardCorners(gray, (self.row,self.column), flags = cv2.CALIB_CB_FAST_CHECK)
             
         else :
-            ret, self.points = cv2.findCirclesGridDefault(gray, (self.row,self.column), flags = cv2.CALIB_CB_SYMMETRIC_GRID + cv2.CALIB_CB_CLUSTERING)
+            ret, self.points = cv2.findCirclesGridDefault(gray, (self.row,self.column))
 
         if ret :
             # Get Sub Pixel
@@ -52,7 +63,30 @@ class ExtrinsicCalibration(object):
             # Get Pose & Positionret, points, rvecs, tvecs = ex_calib.calibrate(img, ex_calib.C
             self.rvecs, self.tvecs, self.inliers = cv2.solvePnPRansac(self.objp, self.points, self.mtx, self.dist)
 
+            # Get 3x3 Rotation Matrix
+            self.rotmx, jac, = cv2.Rodrigues(self.rvecs)
+            # Get eular from Rotation Matrix
+            self.roll, self.pitch, self.yaw = self.rotationMatrixToEulerAngles(self.rotmx)
         return ret
+
+    # Calculates rotation matrix to euler angles
+    # The result is the same as MATLAB except the order
+    # of the euler angles ( x and z are swapped ).
+    def rotationMatrixToEulerAngles(self, R) :
+        sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+     
+        singular = sy < 1e-6
+ 
+        if  not singular :
+            x = math.atan2(R[2,1] , R[2,2])
+            y = math.atan2(-R[2,0], sy)
+            z = math.atan2(R[1,0], R[0,0])
+        else :
+            x = math.atan2(-R[1,2], R[1,1])
+            y = math.atan2(-R[2,0], sy)
+            z = 0
+ 
+        return x, y, z
 
     def imgShow(self, img, ret):
         self.use_window_flg = True
